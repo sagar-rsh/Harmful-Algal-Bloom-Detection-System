ce37c9e102114b5a0094e6a1de9c1a1b96d8c273
Sagar Poojary <55018887+sagar-rsh@users.noreply.github.com>
2025-06-14 17:14:34 +0000

    Tested endpoint on dummy model

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, request, jsonify
 from keras.models import load_model
+import pandas as pd
+import joblib
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Model Loading
+# Model Loading:
 # Load the trained model
-MODEL_PATH = 'models/hab_model.h5' # local path for now. Final version will fetch the model from S3.
+MODEL_PATH = 'models/dummy_hab_model.joblib' # local path for now. Final version will fetch the model from S3.
 try:
-	model = load_model(MODEL_PATH)
+	# model = load_model(MODEL_PATH)
+	model = joblib.load(MODEL_PATH) # Remove this for tensorflow model
 	print("Model loaded successfully.")
 except FileNotFoundError:
 	print(f"Error: Model file not found at {MODEL_PATH}.")
@@ -25,10 +28,32 @@ def predict():
 	
 	# Extract data from the request
 	data = request.json
-	print(data)
+	# print(data)
 
-	return jsonify({"success": "Request received."}), 200
+	required_keys = ['region', 'latitude', 'longitude', 'distance_to_water_m']
+	if not all(key in data for key in required_keys):
+		return jsonify({"error": f"Missing one of the required keys: {required_keys}"}), 400
+	
+	try:
+		input_df = pd.DataFrame([data])
+		input_df = input_df[required_keys]
 
+		prediction_value = int(model.predict(input_df)[0])
+		prediction_proba = model.predict_proba(input_df)[0]
+
+		# Format the response
+		result = {
+			"predicted_severity": prediction_value,
+            "confidence_scores": {
+                str(i): round(float(prob), 4) for i, prob in enumerate(prediction_proba)
+            }
+		}
+
+		return jsonify(result), 200
+
+	except Exception as e:
+		return jsonify({"error": f"An error occurred during prediction: {str(e)}"}), 500
+	
 
 if __name__ == '__main__':
 	# Run the app on port 5000
