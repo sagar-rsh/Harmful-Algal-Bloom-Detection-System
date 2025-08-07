6390866ac1fbebd30aab7c3e6e890ad511eb9ba2
Sagar <poojarysagar09@gmail.com>
2025-08-07 14:42:52 +0100

    Increased threads and misc.

--- backend/config.py
+++ backend/config.py
@@ -2,25 +2,25 @@ TIER_CONFIG = {
     "free": {
         "modalities": ['chlor_a'],
         "days": 5,
-        "threads": 1,
-        "model_path": "models//Free_tier_model.pkl"
+        "threads": 3,
+        "model_path": "models/Free_tier_model.pkl"
     },
     "tier1": {
         "modalities": ['chlor_a', 'Rrs_412', 'Rrs_443'],
         "days": 10,
-        "threads": 4,
-        "model_path": "models//Tier-1_model.h5"
+        "threads": 6,
+        "model_path": "models/Tier-1_model.h5"
     },
     "tier2": {
         "modalities": ['chlor_a', 'Rrs_412', 'Rrs_443'],
         "days": 10,
-        "threads": 8, # Max threads
-        "model_path": "models//Tier-2_model.h5"
+        "threads": 10, # Max threads
+        "model_path": "models/Tier-2_model.h5"
     },
     "admin": {
         "modalities": ['chlor_a', 'Rrs_412', 'Rrs_443'],
         "days": 10,
-        "threads": 8, # Max threads
-        "model_path": "models//Tier-2_model.h5"
+        "threads": 10, # Max threads
+        "model_path": "models/Tier-2_model.h5"
     }
 }
