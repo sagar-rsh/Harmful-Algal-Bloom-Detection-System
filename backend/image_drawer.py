b408be05675518ce570648011edfabec1e362954
Dharmik Vara <dharmikvara62@gmail.com>
2025-08-09 03:34:34 +0100

    added conditions to draw label box inside the bounding box

--- backend/image_drawer.py
+++ backend/image_drawer.py
@@ -23,8 +23,17 @@ def draw_detections_on_image_and_save(input_img: Image.Image, detections: list):
             bbox = draw.textbbox((x1, y1), text, font=font)
             text_width = bbox[2] - bbox[0]
             text_height = bbox[3] - bbox[1]
-            draw.rectangle([x1, y1 - text_height, x1 + text_width, y1], fill="red")
-            draw.text((x1, y1 - text_height), text, fill="white", font=font)
+            
+            if y1 - text_height < 0:
+                # Draw label inside the bounding box at the top
+                label_rect = [x1, y1, x1 + text_width, y1 + text_height]
+                text_pos = (x1, y1)
+            else:
+                # Draw label above the bounding box
+                label_rect = [x1, y1 - text_height, x1 + text_width, y1]
+                text_pos = (x1, y1 - text_height)
+            draw.rectangle(label_rect, fill='red')
+            draw.text(text_pos, text, fill="white", font=font)
 
     if image.mode in ("RGBA", "P"):
         image = image.convert("RGB")
